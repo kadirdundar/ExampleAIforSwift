eb3d671c5c75f7fb2639d2c1f64a6cbc3d4d4111
kadirdundar <100041382+kadirdundar@users.noreply.github.com>
2022-12-09 15:43:20 +0300

    Create Unscale.swift

---
+++ Unscale.swift
@@ -0,0 +1,32 @@
+func scaleData(data: [[Double]]) -> [[Double]] {
+    // Find the minimum and maximum values for each feature.
+    var mins = [Double](repeating: Double.greatestFiniteMagnitude, count: data[0].count)
+    var maxes = [Double](repeating: -Double.greatestFiniteMagnitude, count: data[0].count)
+    for datapoint in data {
+        for i in 0..<datapoint.count {
+            let value = datapoint[i]
+            if value < mins[i] {
+                mins[i] = value
+            }
+            if value > maxes[i] {
+                maxes[i] = value
+            }
+        }
+    }
+    
+    // Scale the data using Min-Max scaling.
+    var scaledData = [[Double]]()
+    for datapoint in data {
+        var scaledDatapoint = [Double]()
+        for i in 0..<datapoint.count {
+            let value = datapoint[i]
+            let min = mins[i]
+            let max = maxes[i]
+            let scaledValue = (value - min) / (max - min)
+            scaledDatapoint.append(scaledValue)
+        }
+        scaledData.append(scaledDatapoint)
+    }
+    
+    return scaledData
+}
