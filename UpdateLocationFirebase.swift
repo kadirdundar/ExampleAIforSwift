83feebbfc9b02b9a359e763bb64cbccedd24ddb1
kadirdundar <100041382+kadirdundar@users.noreply.github.com>
2022-12-12 14:20:39 +0300

    Create UpdateLocationFirebase.swift

---
+++ UpdateLocationFirebase.swift
@@ -0,0 +1,16 @@
+// Firestore database'ine bağlanma işlemi
+let db = Firestore.firestore()
+
+// Eşleşen kaydı bulma ve güncelleme işlemi
+db.collection("collection_name").whereField("email", isEqualTo: email)
+    .getDocuments { (querySnapshot, error) in
+        if let error = error {
+            // Hata mesajı gösterin
+            return
+        } else {
+            for document in querySnapshot!.documents {
+                // Eşleşen kaydı güncelleyin
+                document.reference.updateData(["location": updatedLocation])
+            }
+        }
+    }
