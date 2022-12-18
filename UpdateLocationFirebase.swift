44d1bfbdf71566e5b9d8c3d3109bbc4b616e92f4
kadirdundar <100041382+kadirdundar@users.noreply.github.com>
2022-12-18 15:09:15 +0300

    Update UpdateLocationFirebase.swift

--- UpdateLocationFirebase.swift
+++ UpdateLocationFirebase.swift
@@ -1,17 +1,42 @@
 // Firestore database'ine bağlanma işlemi
-let db = Firestore.firestore()
+        func updateFirestore(aracnumarasi: Int, longitude: Double, latitude: Double, completion: @escaping (Error?) -> Void) {
+          let firestoreDatabase = Firestore.firestore()
+          let location = [GeoPoint(latitude: latitude, longitude: longitude)]
+          let geoPoint = location[0]
+          firestoreDatabase.collection("information").whereField("location", isEqualTo: geoPoint).getDocuments { [self] (snapshot, error) in
+            if let error = error {
+              completion(error)
+              return
+            } else {
+              for document in snapshot!.documents {
+                document.reference.updateData(["arac": aracnumarasi])
+              }
+            }
+            completion(nil)
+          }
+        }
 
-// Eşleşen kaydı bulma ve güncelleme işlemi
-db.collection("collection_name").whereField("email", isEqualTo: email)
-    .getDocuments { (querySnapshot, error) in
-        if let error = error {
-            // Hata mesajı gösterin
-            return
-        } else {
-            for document in querySnapshot!.documents {
-                // Eşleşen kaydı güncelleyin
-                document.reference.updateData(["location": updatedLocation])
+        var aracnumarasi = 1
+        let queue = DispatchQueue(label: "updateQueue")
+
+        for i in unscaleData {
+            queue.sync {
+                aracnumarasi += 1
+                for a in i {
+                    let longitude = a[1]
+                    let latitude = a[0]
+
+                    let semaphore = DispatchSemaphore(value: 0)
+                    updateFirestore(aracnumarasi: aracnumarasi, longitude: longitude, latitude: latitude) { error in
+                        if let error = error {
+                            // Handle the error here
+                            print(error.localizedDescription)
+                        }
+                        
+                        semaphore.signal()
+                        print("getsignal")
+                    }
+                    semaphore.wait()
+                }
             }
         }
-    }
-//denendi başarısız
